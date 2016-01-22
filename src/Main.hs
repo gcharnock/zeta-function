44133f1f6553ebcd5b073d3db1dba05bbf74f28d
Gareth Charnock <gareth.charnock@ocado.com>
2016-01-22 19:09:05 +0000

    Implement complex expansion of the zeta function.

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,16 @@
 
+{-# LANGUAGE FlexibleInstances #-}
+
 module Main where
 
+import Control.Monad
 import Graphics.Rendering.Chart
 import Data.Colour
 import Data.Colour.Names
 import Data.Default.Class
 import Graphics.Rendering.Chart.Backend.Cairo
 import Control.Lens
+import Data.Complex
 
 setLinesBlue :: PlotLines a b -> PlotLines a b
 setLinesBlue = plot_lines_style  . line_color .~ opaque blue
@@ -45,30 +49,41 @@ approxS :: Int -> Double -> Double
 approxS i a = sum $ map (f.fromIntegral) [1..i]
   where f n = n**(-a)
 
-untilConverged :: Double -> [Double] -> Double
+class Abs a where
+  absoluteValue :: a -> Double
+
+instance Abs Double where
+  absoluteValue = abs
+
+instance Abs (Complex Double) where
+  absoluteValue = magnitude
+
+untilConverged :: (Abs a, Floating a, Num a) => Double -> [a] -> a
 untilConverged epsilon (x1:xs) = go x1 xs
   where go last (y:ys) =
-         if abs (last - y) < epsilon
+         if absoluteValue (last - y) < epsilon
            then y
            else go y ys
 
-sumSeries :: (Int -> Double) -> Double
-sumSeries seriesGen = untilConverged 0.000001 $ scanl (+) 0 $ map seriesGen [1..]
+sumSeries :: (Abs a, Floating a, Num a) => (Int -> a) -> a
+sumSeries seriesGen = untilConverged 0.000001 $ tail $ scanl1 (+) $ map seriesGen [1..]
 
 s :: Double -> Double
 s a = sumSeries $ \n-> (fromIntegral n)**(-a)
+
+sDerivative :: (Abs a, Floating a, Num a) => Int -> a -> a
+sDerivative d a = sign * (sumSeries $ \nInt -> 
+    let n = fromIntegral nInt in
+    (fromIntegral nInt)**(-a) * (realToFrac $ log n)^d)
+  where sign = if even d then 1 else (-1)
 
 fact :: Int -> Int
 fact 0 = 1
 fact n = n * fact (n - 1)
 
-sDerivative :: Int -> Double -> Double
-sDerivative d a = (if even d then 1 else (-1)) * (untilConverged 0.000001 $ tail approxes)
-  where approxes = scanl (+) 0 $ map (f.fromIntegral) [1..]
-         where f n = n**(-a) * (log n)^d
-
-taylorTerms :: (Int -> Double -> Double) -> [Double -> Double -> Double]
-taylorTerms = undefined
+taylorCoeffients :: (Fractional a, Num a) => (Int -> a -> a) -> a -> [a]
+taylorCoeffients f a0 = map g [0..]
+  where g n = (f n a0) / fromIntegral (fact n)
 
 taylor1 :: Double -> Double -> Double
 taylor1 a0 a = s a0 + (a - a0) * sDerivative 1 a0
@@ -79,9 +94,7 @@ taylor2 a0 a = s a0 + (a - a0) * sDerivative 1 a0 + 0.5 * (a - a0)^2 * sDerivati
 taylor3 :: Double -> Double -> Double
 taylor3 a0 a = s a0 + (a - a0) * sDerivative 1 a0 + 0.5 * (a - a0)^2 * sDerivative 2 a0 + (1.0/6.0) * (a - a0)^3 * sDerivative 3 a0
 
-wikipediaExampleSeries :: Double -> Double
-wikipediaExampleSeries = undefined
-
 main :: IO ()
-main = putStrLn "Run in ghci"
+main = void $ renderChart
+
 
